389c3ab483b05879e7805934156d557b5a908bdc
abin222 <43913484+abin222@users.noreply.github.com>
2021-09-14 20:49:30 +0530

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,32 @@
 <template>
   <div id="app">
-    <Header
-      :numCorrect="numCorrect"
-      :numTotal="numTotal"
-    />
-
-    <b-container class="bv-example-row">
-      <b-row>
-        <b-col sm="6" offset="3">
-          <QuestionBox
-            v-if="questions.length"
-            :currentQuestion="questions[index]"
-            :next="next"
-            :increment="increment"
-          />
-        </b-col>
-      </b-row>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/cats">Cats</router-link> |
+      <router-link to="/dogs">Dogs</router-link> |
+    </div>
+    <b-container>
+      <router-view/>
     </b-container>
   </div>
 </template>
 
-<script>
-import Header from './components/Header.vue'
-import QuestionBox from './components/QuestionBox.vue'
-
-export default {
-  name: 'app',
-  components: {
-    Header,
-    QuestionBox
-  },
-  data() {
-    return {
-      questions: [],
-      index: 0,
-      numCorrect: 0,
-      numTotal: 0
-    }
-  },
-  methods: {
-    next() {
-      this.index++
-    },
-    increment(isCorrect) {
-      if (isCorrect) {
-        this.numCorrect++
-      }
-      this.numTotal++
-    }
-  },
-  mounted: function() {
-    fetch('https://opentdb.com/api.php?amount=10&category=27&type=multiple', {
-      method: 'get'
-    })
-      .then((response) => {
-        return response.json()
-      })
-      .then((jsonData) => {
-        this.questions = jsonData.results
-      })
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,28 @@
-import '@babel/polyfill'
 import Vue from 'vue'
-import './plugins/vuetify'
-import store from './plugins/store'
-import http from './plugins/axios'
-import moment from 'moment'
+import BootstrapVue from 'bootstrap-vue'
 
-import NoteApp from './NoteApp.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/sass/index.sass'
+
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCat, faDog } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faCat, faDog);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
-Vue.filter('date', time => moment(time)
-  .format('DD/MM/YY, HH:mm'))
-
 new Vue({
+  router,
   store,
-  http,
-  render: h => h(NoteApp)
-}).$mount('#project-notes')
+  render: h => h(App)
+}).$mount('#app')
